9514508c2cf8991c134ce982630093860c53e88e
dbolotov <dbolotov@live.com>
2012-06-27 23:48:00 -0700

    fixed functions, formatted results output

--- lin_reg.py
+++ lin_reg.py
@@ -1,16 +1,23 @@
 #!/opt/EPD/bin/python
 #Filename: lin_reg.py
-#perform simple linear regression, compute training and test error, predict on test set
+
+#perform simple linear regression using batch gradient descent on training and test sets 
+#compute training and test error, predict on test set
 #code ported from linear_regression_script.m
 
 
 import numpy
-from numpy import mat, c_
+from numpy import mat, c_, array
 
 #Define functions
 def gradientDescentMulti(X, y, theta, alpha, num_iters):
 	m = y.shape[0]
-	J_history = mat(numpy.zeros((num_iters,1)))
+	J_history = numpy.zeros((num_iters,1))
+	for iter in range(0,num_iters):
+		#perform a single gradient step on the parameter vector theta
+		theta = theta - alpha/m*(numpy.transpose(X)*(X*theta-y))
+		#save the cost J in every iteration
+		J_history[iter] = computeCostMulti(X,y,theta)
 	return theta,J_history
 
 def computeCostMulti(X,y,theta):
@@ -23,15 +30,13 @@ data = numpy.loadtxt('simple_function_1.txt', delimiter=',')
 
 #gradient descent parameters
 alpha = 0.01
-num_iters = 1000.
+num_iters = 1000
 
 #percentage of data to use for training
 train_perc = 0.95
 
 #separate input file into independent and dependent arrays
-X = mat(data[:,:2]) #import as matrix
-###X = data[:,:2] #import as array
-
+X = mat(data[:,:2]) 
 y = numpy.transpose(mat(data[:,2]))
 
 #split into training and test sets
@@ -63,13 +68,14 @@ theta = mat(numpy.zeros((X.shape[1],1)))
 theta,J_history = gradientDescentMulti(X,y,theta,alpha,num_iters)
 
 #Plot the GD convergence 
+#(not done)
 
 #Display GD result
 print "Theta computed from gradient descent:\n",theta
 
 #Display cost function intial and end values
-print "Cost function start:\n"
-print "Cost function end:\n"
+print "Cost function start: %g \n" % J_history[0]
+print "Cost function end: %g \n" % J_history[-1]
 
 #Use test set for prediction
 y_hat = X_test*theta
@@ -83,10 +89,7 @@ J_test = computeCostMulti(X_test,y_test,theta)
 #Print test set variable - actual and prediction
 print "Linear fit: actual vs. prediction\n"
 print c_[y_test,y_hat]
-print "Training set error: %f \n" % J_train
-print "Test set error: %f \n" % J_test
+print "Training set error: %g \n" % J_train
+print "Test set error: %g \n" % J_test
 #EOF
-
-
-
 
